973bcb7fe5a9d5e38302339c17fffe2487cb5a0c
rachel-lawrie <124215524+rachel-lawrie@users.noreply.github.com>
2024-09-26 14:55:13 -0700

    Adjust port to accept calls from Expo

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def register_user():
     return jsonify({"message": "User registered successfully!"}), 201
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5001)
